d0c4dfb0c94c2026fe75f0b294a1318dcca12cb1
Alla-Nazarenko <umnyj.start@gmail.com>
2025-03-21 12:05:50 +0100

    car + enums

---
+++ src/main/java/de/aittr/car_rent/domain/entity/CarFuelType.java
@@ -0,0 +1,16 @@
+package de.aittr.car_rent.domain.entity;
+
+public enum CarFuelType {
+
+    PETROL,
+    DIESEL,
+    ELECTRIC,
+    HYBRID,
+    //(LIQUEFIED PETROLEUM GAS)
+    LPG,
+    //(COMPRESSED NATURAL GAS)
+    CNG,
+    BIOFUEL,
+    HYDROGEN,
+    ETHANOL
+}
