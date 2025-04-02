71568815fc68742218dfa40bbc641c0f7ae2ac8c
KKibirieva <kkibirieva@gmail.com>
2025-04-02 10:10:54 +0200

    customer dto upd

--- src/main/java/de/aittr/car_rent/domain/dto/CustomerUpdateRequestDto.java
+++ src/main/java/de/aittr/car_rent/domain/dto/CustomerUpdateRequestDto.java
@@ -1,7 +1,20 @@
 package de.aittr.car_rent.domain.dto;
 
-public record CustomerUpdateRequestDto(String firstName,
-                                       String lastName,
-                                       String email
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
+public record CustomerUpdateRequestDto(
+
+        @NotBlank(message = "{customer.firstName.notBlank}")
+        @Schema(description = "Customer's first name", example = "John")
+        String firstName,
+
+        @NotBlank(message = "{customer.lastName.notBlank}")
+        @Schema(description = "Customer's last name", example = "Doe")
+        String lastName,
+
+        @NotBlank(message = "{customer.email.notBlank}")
+        @Schema(description = "Customer's email address", example = "john.doe@example.com")
+        String email
 
 ) {}
